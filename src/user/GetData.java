f01adf1328189e516f06a8b29db7158f04b5d6eb
Marcelino Gil Nombela <112982522+MGN3@users.noreply.github.com>
2023-05-08 18:10:51 +0200

    Update GetData.java

--- src/user/GetData.java
+++ src/user/GetData.java
@@ -21,10 +21,18 @@ public class GetData {
 	 * @return newTech The object created with the console data.
 	 */
 	public static Technology newTech() {
-		Technology newTech = new Technology(Utilities.getString("Set Technology name: "),
-				Utilities.getString("Set Technology type: "), Utilities.getString("Set Technology purpose: "),
-				Utilities.getIntBetween(1900, 2050, "Introduce the Technology release year: "),
-				Utilities.getString("Set Technology license: "));
+
+		Technology newTech = new Technology(null, null, null, 0, null);
+
+		try {
+			newTech = new Technology(Utilities.getString("Set Technology name: "),
+					Utilities.getString("Set Technology type: "), Utilities.getString("Set Technology purpose: "),
+					Utilities.getIntBetween(1900, 2050, "Introduce the Technology release year: "),
+					Utilities.getString("Set Technology license: "));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return newTech;
 	}
 
@@ -37,9 +45,17 @@ public class GetData {
 	 * @return newWeb The object created with the console data.
 	 */
 	public static WebPage newWeb() {
-		WebPage newWeb = new WebPage(Utilities.getString("Introduce Webpage name: "),
-				Utilities.getString("Introduce the URL: "),
-				Utilities.getIntBetween(1, 999999999, "Introduce the budget for the web project: "));
+
+		WebPage newWeb = new WebPage(null, null, 0);
+
+		try {
+			newWeb = new WebPage(Utilities.getString("Introduce Webpage name: "),
+					Utilities.getString("Introduce the URL: "),
+					Utilities.getIntBetween(1, 999999999, "Introduce the budget for the web project: "));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return newWeb;
 	}
 
@@ -54,31 +70,35 @@ public class GetData {
 		String chosenField = "techName";
 		int columnNumber;
 
-		columnNumber = Utilities.getIntBetween(1, 5,
-				"Enter 1, 2, 3, 4 or 5 to search a tech by: id, name, type, purpose, release year or license");
-		switch (columnNumber) {
-		case 1:
-			chosenField = "techid";
-			break;
+		try {
+			columnNumber = Utilities.getIntBetween(1, 5,
+					"Enter 1, 2, 3, 4 or 5 to search a tech by: id, name, type, purpose, release year or license");
+			switch (columnNumber) {
+			case 1:
+				chosenField = "techid";
+				break;
 
-		case 2:
-			chosenField = "techName";
-			break;
+			case 2:
+				chosenField = "techName";
+				break;
 
-		case 3:
-			chosenField = "techType";
-			break;
+			case 3:
+				chosenField = "techType";
+				break;
 
-		case 4:
-			chosenField = "purpose";
-			break;
+			case 4:
+				chosenField = "purpose";
+				break;
 
-		case 5:
-			chosenField = "releaseYear";
-			break;
-		case 6:
-			chosenField = "license";
-			break;
+			case 5:
+				chosenField = "releaseYear";
+				break;
+			case 6:
+				chosenField = "license";
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 		return chosenField;
@@ -95,26 +115,30 @@ public class GetData {
 		String chosenField = "techName";
 		int columnNumber;
 
-		columnNumber = Utilities.getIntBetween(1, 5, "Enter 1, 2, 3 or 4 to search a web by: id, name, url or budget.");
-		switch (columnNumber) {
-		case 1:
-			chosenField = "webid";
-			break;
+		try {
+			columnNumber = Utilities.getIntBetween(1, 5,
+					"Enter 1, 2, 3 or 4 to search a web by: id, name, url or budget.");
+			switch (columnNumber) {
+			case 1:
+				chosenField = "webid";
+				break;
 
-		case 2:
-			chosenField = "webName";
-			break;
+			case 2:
+				chosenField = "webName";
+				break;
 
-		case 3:
-			chosenField = "url";
-			break;
+			case 3:
+				chosenField = "url";
+				break;
 
-		case 4:
-			chosenField = "budget";
-			break;
+			case 4:
+				chosenField = "budget";
+				break;
 
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
 		return chosenField;
 	}
 
